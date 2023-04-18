6e66c2b54630aacf63542eca5089124f75978785
saif <saifbahr54@gmail.com>
2023-04-19 00:26:59 +0100

    material

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  checked = true;
+  modCheck(){
+    this.checked=!this.checked;
+  }
 }
