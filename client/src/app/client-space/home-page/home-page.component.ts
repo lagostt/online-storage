5e5e4b2b4b4e0a7b2a67f3842e72b8da0e7f9338
saif <saifbahr54@gmail.com>
2023-05-01 18:17:21 +0100

    added home page background and user avatar with menu

--- client/src/app/client-space/home-page/home-page.component.ts
+++ client/src/app/client-space/home-page/home-page.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
 
+  userMenuIsOpen: boolean = false;
+  
+  handleUserMenu(){
+    this.userMenuIsOpen = !this.userMenuIsOpen; 
+  }
+
+  ngOnInit(): void {
+    document.body.style.backgroundImage="linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)";
+  }
 }
