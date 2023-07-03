8e92f9615d8c749270a0d697e575d1ed35abedfc
saif <saifbahr54@gmail.com>
2023-07-03 18:32:56 +0100

    file manager ui improvments

--- client/src/app/components/file-manager/file-manager.component.ts
+++ client/src/app/components/file-manager/file-manager.component.ts
@@ -8,12 +8,12 @@ import { OnInit } from '@angular/core';
 export class FileManagerComponent implements OnInit {
 
   userMenuIsOpen: boolean = true;
-  items:Array<String>=['item1','item2','item3','item4','item5','item6','item7','item8','item9','item10','item11'];
+  items:Array<String>= Array.from(Array(100).keys()).map(x => 'item'+ x);
   handleUserMenu(){
     this.userMenuIsOpen = !this.userMenuIsOpen; 
   }
 
   ngOnInit(): void {
-    document.body.style.backgroundImage="linear-gradient( 135deg, #ABDCFF 10%, #0396FF 100%)";
+    document.body.style.backgroundColor="#abdcff";
   }
 }
